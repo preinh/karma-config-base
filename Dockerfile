d42199144981bae8bbb1fb3a153414604e38466b
Marlon Pirchiner <marlon.pirchiner@csiro.au>
2016-02-19 13:22:28 +1100

    just starting

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM ubuntu
+ENV HOME /root
+# ADD . /root/karma
+RUN apt-get update && apt-get install -y --no-install-recommends curl wget git sysv-rc-conf openjdk-7-jdk maven 
+
+RUN cd /opt && \
+	git clone https://github.com/usc-isi-i2/Web-Karma.git
+
+RUN cd /opt/Web-Karma && \
+	mvn clean install -DskipTests
+#	mvn clean install
+
+EXPOSE 8080
+
+ENTRYPOINT ["cd /opt/Web-Karma/karma-web && mvn jetty:run "]
+CMD ["/bin/bash"]
+
+
+# TODO: 
+
+# 	- add main ontologies (DC, SKOS, ... to the repo)
+# 	- set up preload_ontologies from the Dockerfile
+# 	- include test some test files (CSV, XLS, ...)
+
+
---
+++ preload_ontologies_download.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+PRELOAD_DIR=preloaded_ontologies
+
+# SKOS CORE
+wget "https://www.w3.org/2009/08/skos-reference/skos.rdf"
+mv skos.rdf $PRELOAD_DIR
+
+# SKOS-XL
+wget "https://www.w3.org/2009/08/skos-reference/skos-xl.rdf"
+mv skos-xl.rdf $PRELOAD_DIR
+
+# FOAF
+wget "http://xmlns.com/foaf/spec/index.rdf"
+mv index.rdf $PRELOAD_DIR/foaf_latest.rdf
+
+# DC
+wget "http://dublincore.org/2012/06/14/dcelements.rdf"
+mv dcelements.rdf $PRELOAD_DIR
+
+# DC-TERM
+wget "http://dublincore.org/2012/06/14/dcterms.rdf"
+mv dcterms.rdf $PRELOAD_DIR
+
+
+# PROV
+wget "http://www.w3.org/ns/prov-o"
+mv prov-o $PRELOAD_DIR/prov-o.ttl
+
+# QUDT
+wget -O osg-qudt-1.01.ttl \
+	 "http://qudt.org/1.1/schema/OSG_qudt-(v1.01).ttl"
+mv osg-qudt-1.01.ttl $PRELOAD_DIR
+
+# QUDT - Quantity
+wget -O osg-quantity-1.1.ttl \
+	 "http://qudt.org/1.1/schema/OSG_quantity-(v1.1).ttl"
+mv osg-quantity-1.1.ttl $PRELOAD_DIR
+
+# OP
+wget -O observable_properties-no_meta.rdf \
+	 "http://environment.data.gov.au/def/op?_format=rdf"
+mv observable_properties-no_meta.rdf $PRELOAD_DIR
